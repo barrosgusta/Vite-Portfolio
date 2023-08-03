a6fd422ec50a24d7b1d8aabf5c78efb4abad16c9
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-08-03 15:32:20 -0300

    refactor image miniature component

--- src/components/ImageMiniature/ImageMiniatureRoot.tsx
+++ src/components/ImageMiniature/ImageMiniatureRoot.tsx
@@ -15,7 +15,7 @@ export function ImageMiniatureRoot({imageSource, projectUrl, siteUrl, title, cla
     return (
         <figure className={twMerge("group duration-300 relative rounded-xl w-96 border border-indigo-400 shadow-default overflow-hidden hover:scale-110", className)}>
             {children}
-            <h4 className='p-1 text-center text-zinc-800 font-sfpmedium bg-zinc-100 shadow-md z-20 dark:text-zinc-100 dark:bg-zinc-800'>
+            <h4 className='p-1 text-center text-zinc-800 font-semibold bg-zinc-100 shadow-md z-20 dark:text-zinc-100 dark:bg-zinc-800'>
                 {title}
             </h4>
             <div className='opacity-0 group-hover:opacity-100 duration-300 absolute right-0 p-2 z-10'>
