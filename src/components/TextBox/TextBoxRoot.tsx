0c91d9ecb318fb58fd03b5a78475046f78193d1c
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-02 00:07:22 -0300

    Update TextBoxRoot.tsx

--- src/components/TextBox/TextBoxRoot.tsx
+++ src/components/TextBox/TextBoxRoot.tsx
@@ -8,7 +8,7 @@ interface TextBoxRootProps {
 
 export function TextBoxRoot({ children, className = "border-zinc-700 bg-zinc-800" }: TextBoxRootProps) {
     return (
-        <div className={twMerge("py-5 rounded-xl backdrop-blur-3xl bg-opacity-70 border shadow-default", className)} >
+        <div className={twMerge("py-5 rounded-xl backdrop-blur-3xl border shadow-default", className)} >
             <div className="flex justify-center items-center">
                 {children}
             </div>
