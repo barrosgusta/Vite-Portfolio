00c7c9340b9a36d5885f34d4805ccf7ec631f01b
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-02 00:07:40 -0300

    create the Project Modal

---
+++ src/components/ProjectModal.tsx
@@ -0,0 +1,98 @@
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import ImageCarousel from "./ImageCarousel";
+import { ReactNode } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { PillSeparator } from "./PillSeparator";
+import { IconFrame } from "./IconFrame/IconFrame";
+import { GithubIcon, Globe2 } from "lucide-react";
+
+type ProjectModalProps = {
+    imagesSource: string[];
+    projectUrl?: string;
+    siteUrl?: string;
+    onClose: () => void;
+    isOpen: boolean;
+    children: ReactNode;
+}
+
+export function ProjectModalHeader({ children }: { children: ReactNode }) {
+    return (
+        <div className="flex flex-col text-center lg:text-left">
+            {children}
+        </div>
+    )
+}
+
+export function ProjectModalTitle({ children }: { children: ReactNode }) {
+    return (
+        <h3 className="mb-3 text-lg font-semibold leading-none dark:text-white">
+            {children}
+        </h3>
+    )
+}
+
+export function ProjectModalDescription({ children }: { children: ReactNode }) {
+    return (
+        <p className="dark:text-zinc-300">
+            {children}
+        </p>
+    )
+}
+
+export function ProjectModalTechnologies({ children }: { children: ReactNode }) {
+    return (
+        <ScrollArea className="flex flex-col items-center justify-center bg-white dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-800 shadow-default rounded-2xl max-h-72">
+            <div className="p-4">
+                <div className="">
+                    <h3 className="text-lg text-center font-bold leading-none dark:text-white">
+                        Tecnologias
+                    </h3>
+                </div>
+                <PillSeparator className="mt-3 mb-1" />
+                <section className="grid grid-cols-1">
+                    {children}
+                </section>
+            </div>
+        </ScrollArea>
+    )
+}
+
+export function ProjectModalContent({ children }: { children: ReactNode }) {
+    return (
+        <div className="flex flex-col lg:justify-between w-full h-full gap-4">
+            {children}
+        </div>
+    )
+}
+
+export function ProjectModal({ imagesSource, projectUrl, siteUrl, onClose, isOpen, children }: ProjectModalProps) {
+    const onOpenChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    }
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onOpenChange}>
+            <DialogContent className="w-full h-screen lg:h-auto lg:max-h-[60vh] min-h-fit p-0">
+                <ScrollArea>
+                    <div className="m-6">
+                        {(projectUrl || siteUrl) && (<div className="hidden lg:flex items-center justify-center h-28 backdrop-blur-3xl fixed left-[50%] top-[-35%] z-50 max-w-fit translate-x-[-50%] border border-indigo-500 bg-white/75 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-xl md:w-full dark:bg-zinc-900/75">
+                            {projectUrl && (<IconFrame href={projectUrl} isSmall icon={<GithubIcon size={35} />} className="w-16 h-16 rounded-xl" />)}
+                            {siteUrl && (<IconFrame href={siteUrl} isSmall icon={<Globe2 size={35} />} className="w-16 h-16 rounded-xl" />)}
+                        </div>)}
+                        <div className="grid lg:grid-cols-2 mb-auto gap-4 items-center lg:flex-row">
+                            {(projectUrl || siteUrl) && (<div className="lg:hidden flex items-center justify-center">
+                                {projectUrl && (<IconFrame href={projectUrl} isSmall icon={<GithubIcon size={35} />} className="w-16 h-16 rounded-xl" />)}
+                                {siteUrl && (<IconFrame href={siteUrl} isSmall icon={<Globe2 size={35} />} className="w-16 h-16 rounded-xl" />)}
+                            </div>)}
+                            <ImageCarousel className="aspect-[15/9] lg:aspect-[14/9]" images={imagesSource} />
+                            <PillSeparator className="lg:hidden" />
+                            {children}
+                        </div>
+                    </div>
+                </ScrollArea>
+            </DialogContent>
+        </Dialog>
+    )
+}
