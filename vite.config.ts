315d213397ec109f6ccf50e93b43634585417b9e
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-02 00:06:15 -0300

    add Shadcn/UI components

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,5 +9,10 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 5173
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
