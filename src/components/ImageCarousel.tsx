c3b78126444c049984954d4c457cad4162155f62
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-09-29 13:57:52 -0300

    increase arrows contrast

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -27,22 +27,22 @@ export default function ImageCarousel({ images, className }: ImageCarouselProps)
     }
 
     return (
-        <div className={twMerge("overflow-hidden aspect-[14/9] relative rounded-xl shadow-inner border border-zinc-300 dark:border-zinc-800", className)}>
+        <div className={twMerge("overflow-hidden aspect-video relative rounded-xl shadow-inner border border-zinc-300 dark:border-zinc-800", className)}>
             <div 
                 className="flex h-full w-full transition-transform duration-500"
                 style={{ transform: `translateX(-${currImage * 100}%)` }}
             >
                 {images.map((image) => (
-                    <img className="block object-cover " key={image} src={image} alt="image" />
+                    <img className="block object-cover object-center" key={image} src={image} alt="image" />
                 ))}
             </div>
             <div className={twMerge("absolute inset-0 flex items-center justify-between duration-300", images.length === 1 && "hidden")}>
                 <button onClick={handlePrevImage} className="group flex items-center justify-start h-full w-24">
-                    <ArrowLeft className="ml-4" color="white" />
+                    <ArrowLeft className="ml-4 mix-blend-difference" size={30} color="white" />
                     <div className="opacity-0 group-hover:opacity-30 absolute top-0 duration-300 bg-gradient-to-r from-zinc-700 h-full w-36" />
                 </button>
                 <button onClick={handleNextImage} className="group flex items-center justify-end h-full w-24" >
-                    <ArrowRight className="mr-4" color="white" />
+                    <ArrowRight className="mr-4 mix-blend-difference" size={30} color="white" />
                     <div className="opacity-0 group-hover:opacity-30 absolute top-0 duration-300 bg-gradient-to-l from-zinc-700 h-full w-36" />
                 </button>
             </div>
