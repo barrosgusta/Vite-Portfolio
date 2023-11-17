6fea91c81d29538dd000c3e9158cefeb089d6e5a
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-16 23:25:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,6 @@ function App() {
         <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start gap-4 xl:gap-7 transition-all duration-75">
           <SideBar />
           <MainForm />
-          <div className="md:hidden flex justify-center backdrop-blur-3xl backdrop-saturate-200 z-10 rounded-xl bg-zinc-100 bg-opacity-60 h-fit w-full border border-zinc-400 shadow-default
-          dark:bg-zinc-800 dark:bg-opacity-80 dark:border-zinc-700">
-            <div className="m-9">
-              <SideBarInfo />
-            </div>
-          </div>
         </div>
       </div>
 
