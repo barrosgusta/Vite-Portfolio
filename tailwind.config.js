f7fde41a8ba824cc4b458c7b90cc44427f898571
barrosgusta <59765082+barrosgusta@users.noreply.github.com>
2023-11-21 13:12:45 -0300

    add new boxShadow theme and svg animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,17 +27,30 @@ module.exports = {
         },
         "breathe": {
           "0%, 100%": { opacity: 1, transform: "scale(1)" },
-          "50%": { opacity: 0.5, transform: "scale(1.2)"  },
+          "50%": { opacity: 0.8, transform: "scale(1.2)"  },
         }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "breathe": "breathe 30s infinite",
+
+        // Background svg animations
+        "bg-breathe-1": "breathe 30s infinite",
+        "bg-breathe-2": "breathe 25s infinite",
+        "bg-breathe-3": "breathe 32s infinite",
+        "bg-breathe-4": "breathe 22s infinite",
+        "bg-breathe-5": "breathe 16s infinite",
+        "bg-breathe-6": "breathe 28s infinite",
+        "bg-breathe-7": "breathe 34s infinite",
+        "bg-breathe-8": "breathe 20s infinite",
+        "bg-breathe-9": "breathe 28s infinite",
+        "bg-breathe-10": "breathe 18s infinite",
+        "bg-breathe-11": "breathe 24s infinite",
+        "bg-breathe-12": "breathe 29s infinite",
       },
       boxShadow: {
         'default': '0 0px 25px 0px rgb(50, 50, 50, 0.3)',
-        'default-smaller': '0 0px 4px 0px rgb(35, 35, 35, 0.3)',
+        'default-sm': '0 0px 4px 0px rgb(35, 35, 35, 0.3)',
       },
     },
   },
