3f5686a1b16e468d6c2f3d13235810e9ec3124e8
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-01-16 18:00:03 -0300

    refactor repeated code and change the ui

--- src/components/main-window.tsx
+++ src/components/main-window.tsx
@@ -1,15 +1,14 @@
 import { AnimatedRoutes } from "./animated-routes";
 import { NavBarWindow } from "./nav-bar/nav-bar-window";
+import WindowGlass from "./ui/window-glass";
 
 export default function MainWindow() {
     return (
-        <div className="p-5 md:p-7
-        relative backdrop-blur-lg backdrop-saturate-200 backdrop-brightness-150 dark:backdrop-brightness-75 backdrop-contrast-125 z-10 w-full rounded-xl bg-zinc-100/60 border border-zinc-300/60 shadow-default
-        dark:bg-zinc-800/60 dark:border-zinc-700/60 transition-transform duration-1000">
+        <WindowGlass className="p-5 md:p-7 relative z-10 w-full transition-transform duration-1000">
                 <div className="absolute right-0 -translate-y-7">
                     <NavBarWindow />
                 </div>
                 <AnimatedRoutes />
-        </div>
+        </WindowGlass>
     );
 }
---
+++ src/components/ui/window-glass.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+
+type WindowGlassProps = {
+    children: ReactNode,
+    className?: string
+};
+
+export default function WindowGlass({children, className}: WindowGlassProps) {
+    return (
+        <div 
+            className={cn(
+                "backdrop-blur-lg",
+                "backdrop-saturate-200",
+                "dark:backdrop-saturate-100",
+                "backdrop-brightness-150",
+                "backdrop-contrast-125",
+                "dark:backdrop-contrast-100",
+                "rounded-xl",
+                "bg-zinc-100/60",
+                "border-2",
+                "dark:border",
+                "border-indigo-500/60",
+                "shadow-default",
+                "dark:bg-zinc-950/60",
+                className
+            )}
+        >
+            {children}
+        </div>
+    )
+}
