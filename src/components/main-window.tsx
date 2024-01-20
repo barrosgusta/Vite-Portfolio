d7d7ec26a180edd716b74960f2adb802872ee4b4
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-01-20 16:02:12 -0300

    Update main-window.tsx

--- src/components/main-window.tsx
+++ src/components/main-window.tsx
@@ -4,7 +4,7 @@ import WindowGlass from "./ui/window-glass";
 
 export default function MainWindow() {
     return (
-        <WindowGlass className="p-5 md:p-7 relative z-10 w-full transition-transform duration-1000">
+        <WindowGlass className="p-5 md:p-7 relative w-full transition-transform duration-1000">
                 <div className="absolute right-0 -translate-y-7">
                     <NavBarWindow />
                 </div>
