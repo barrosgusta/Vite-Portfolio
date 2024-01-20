44032148623509a49cae7fc2f834d446f86cbcd1
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-01-20 16:01:30 -0300

    implement i18next and change border

--- src/components/tech-box.tsx
+++ src/components/tech-box.tsx
@@ -1,22 +1,41 @@
-import { TextBox } from "@/components/text-box"
-import { faReact } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { BiLogoJavascript, BiLogoTypescript, BiLogoDocker, BiLogoPython, BiLogoMongodb, BiLogoPostgresql, BiLogoPhp, BiLogoNodejs, BiLogoTailwindCss } from "react-icons/bi"
-import { SiNextdotjs, SiDelphi, SiPrisma } from "react-icons/si"
-import { TbBrandMysql, TbBrandRadixUi, TbBrandRust, TbBrandStripe, TbBrandSwift } from "react-icons/tb"
-import { RiOpenaiFill, RiSupabaseLine } from "react-icons/ri"
-import { ShadcnUiIcon } from "@/components/icons/shadcn-ui-icon"
+import { TextBox } from "@/components/text-box";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BiLogoJavascript, BiLogoTypescript, BiLogoDocker, BiLogoPython, BiLogoMongodb, BiLogoPostgresql, BiLogoPhp, BiLogoNodejs, BiLogoTailwindCss } from "react-icons/bi";
+import { SiNextdotjs, SiDelphi, SiPrisma, SiNestjs, SiPhp } from "react-icons/si";
+import { TbBrandMysql, TbBrandRadixUi, TbBrandRust, TbBrandStripe, TbBrandSwift } from "react-icons/tb";
+import { RiOpenaiFill, RiSupabaseLine } from "react-icons/ri";
+import { ShadcnUiIcon } from "@/components/icons/shadcn-ui-icon";
+import i18n from "@/lib/i18n";
+
+const t = i18n.t;
 
 function ReactJs() {
     return (
-        <TextBox.Root className="bg-sky-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-sky-500">
             <TextBox.Icon icon={<FontAwesomeIcon icon={faReact} />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     React.js
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Biblioteca JavaScript
+                    {t("tech.reactjs")}
+                </TextBox.Content.Description>
+            </TextBox.Content.Root>
+        </TextBox.Root>
+    )
+}
+
+function NestJS() {
+    return (
+        <TextBox.Root className="bg-black">
+            <TextBox.Icon icon={<SiNestjs />} />
+            <TextBox.Content.Root>
+                <TextBox.Content.Title className="text-white">
+                    Nest.js
+                </TextBox.Content.Title>
+                <TextBox.Content.Description className="text-zinc-100 text-sm">
+                    {t("tech.nestjs")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -25,14 +44,14 @@ function ReactJs() {
 
 function NextJs() {
     return (
-        <TextBox.Root className="bg-black border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-black">
             <TextBox.Icon icon={<SiNextdotjs />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Next.js
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Framework React
+                    {t("tech.nextjs")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -41,14 +60,14 @@ function NextJs() {
 
 function JavaScript() {
     return (
-        <TextBox.Root className="bg-[rgb(232,235,78)] border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-[rgb(232,235,78)]">
             <TextBox.Icon className="text-black" icon={<BiLogoJavascript />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-black">
                     JavaScript
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-900 text-sm">
-                    Linguagem interpretada
+                    {t("tech.javascript")}                
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -57,14 +76,14 @@ function JavaScript() {
 
 function TypeScript() {
     return (
-        <TextBox.Root className="bg-sky-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-sky-500">
             <TextBox.Icon icon={<BiLogoTypescript />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     TypeScript
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Linguagem tipada
+                    {t("tech.typescript")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -73,14 +92,14 @@ function TypeScript() {
 
 function Docker() {
     return (
-        <TextBox.Root className="bg-sky-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-sky-600">
             <TextBox.Icon icon={<BiLogoDocker />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Docker
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Plataforma de virtualização
+                    {t("tech.docker")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -89,14 +108,14 @@ function Docker() {
 
 function Python() {
     return (
-        <TextBox.Root className="bg-gradient-to-tl from-yellow-600 to-blue-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-gradient-to-tl from-yellow-600 to-blue-600">
             <TextBox.Icon icon={<BiLogoPython />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Python
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Linguagem alto nível
+                    {t("tech.python")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -105,14 +124,14 @@ function Python() {
 
 function Delphi() {
     return (
-        <TextBox.Root className="bg-red-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-red-500">
             <TextBox.Icon icon={<SiDelphi />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Delphi
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Desenvolvimento Desktop
+                    {t("tech.delphi")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -121,14 +140,14 @@ function Delphi() {
 
 function MongoDb() {
     return (
-        <TextBox.Root className="bg-green-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-green-600">
             <TextBox.Icon icon={<BiLogoMongodb />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     MongoDB
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Banco de dados não relacional
+                    {t("tech.mongodb")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -137,14 +156,14 @@ function MongoDb() {
 
 function PostgreSql() {
     return (
-        <TextBox.Root className="bg-blue-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-blue-600">
             <TextBox.Icon icon={<BiLogoPostgresql />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     PostgreSQL
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Banco de dados relacional
+                    {t("tech.postgresql")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -153,14 +172,14 @@ function PostgreSql() {
 
 function Php() {
     return (
-        <TextBox.Root className="bg-blue-500 border-zinc-300/60 dark:border-zinc-700/60">
-            <TextBox.Icon icon={<BiLogoPhp />} />
+        <TextBox.Root className="bg-blue-500">
+            <TextBox.Icon icon={<SiPhp />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     PHP
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Backend Web
+                    {t("tech.php")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -169,14 +188,14 @@ function Php() {
 
 function NodeJs() {
     return (
-        <TextBox.Root className="bg-green-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-green-500">
             <TextBox.Icon icon={<BiLogoNodejs />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Node.js
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Runtime JavaScript
+                    {t("tech.nodejs")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -185,14 +204,14 @@ function NodeJs() {
 
 function ShadcnUi() {
     return (
-        <TextBox.Root className="bg-black border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-black">
             <TextBox.Icon icon={<ShadcnUiIcon />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Shadcn/UI
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Biblioteca de componentes React
+                    {t("tech.shadcnui")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -201,14 +220,14 @@ function ShadcnUi() {
 
 function RadixUi() {
     return (
-        <TextBox.Root className="bg-black border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-black">
             <TextBox.Icon icon={<TbBrandRadixUi />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     RadixUI
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Biblioteca de componentes React
+                    {t("tech.radixui")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -217,14 +236,14 @@ function RadixUi() {
 
 function TailwindCss() {
     return (
-        <TextBox.Root className="bg-cyan-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-cyan-500">
             <TextBox.Icon icon={<BiLogoTailwindCss />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     TailwindCSS
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Biblioteca de classes CSS
+                    {t("tech.tailwindcss")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -233,14 +252,14 @@ function TailwindCss() {
 
 function OpenAi() {
     return (
-        <TextBox.Root className="bg-indigo-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-indigo-500">
             <TextBox.Icon icon={<RiOpenaiFill />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     OpenAI API
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    API de inteligência artificial
+                    {t("tech.openai")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -249,14 +268,14 @@ function OpenAi() {
 
 function Supabase() {
     return (
-        <TextBox.Root className="bg-green-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-green-500">
             <TextBox.Icon icon={<RiSupabaseLine />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Supabase
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Plataforma de desenvolvimento
+                    {t("tech.supabase")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -265,14 +284,14 @@ function Supabase() {
 
 function Stripe() {
     return (
-        <TextBox.Root className="bg-purple-500 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-purple-500">
             <TextBox.Icon icon={<TbBrandStripe />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Stripe
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Plataforma de pagamentos
+                    {t("tech.stripe")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -281,14 +300,14 @@ function Stripe() {
 
 function MySql() {
     return (
-        <TextBox.Root className="bg-gradient-to-tl from-orange-600 to-blue-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-gradient-to-tl from-orange-600 to-blue-600">
             <TextBox.Icon icon={<TbBrandMysql />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     MySQL
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Banco de dados relacional
+                    {t("tech.mysql")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -297,14 +316,14 @@ function MySql() {
 
 function Swift() {
     return (
-        <TextBox.Root className="bg-orange-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-orange-600">
             <TextBox.Icon icon={<TbBrandSwift />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Swift
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Linguagem nativa Apple
+                    {t("tech.swift")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -313,14 +332,14 @@ function Swift() {
 
 function Prisma() {
     return (
-        <TextBox.Root className="bg-indigo-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-indigo-600">
             <TextBox.Icon className="text-white" icon={<SiPrisma />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Prisma
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                   DB ORM
+                    {t("tech.prisma")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -329,14 +348,14 @@ function Prisma() {
 
 function Rust() {
     return (
-        <TextBox.Root className="bg-orange-600 border-zinc-300/60 dark:border-zinc-700/60">
+        <TextBox.Root className="bg-orange-600">
             <TextBox.Icon icon={<TbBrandRust />} />
             <TextBox.Content.Root>
                 <TextBox.Content.Title className="text-white">
                     Rust
                 </TextBox.Content.Title>
                 <TextBox.Content.Description className="text-zinc-100 text-sm">
-                    Linguagem alto nível
+                    {t("tech.rust")}
                 </TextBox.Content.Description>
             </TextBox.Content.Root>
         </TextBox.Root>
@@ -346,6 +365,7 @@ function Rust() {
 
 const TechBoxes = {
     ReactJs,
+    NestJS,
     NextJs,
     JavaScript,
     TypeScript,
